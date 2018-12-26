a37236df818ce89a29e9b0d4200d17d8aa485b62
Jason-ZW <zhenyang@rancher.com>
2018-12-26 16:30:20 +0800

    Add index page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,24 @@
 "use strict";
 Page({
-
+    data: {
+        searchValue: '',
+    },
+    onSearchChange(e) {
+        this.setData({
+            value: e.detail.value,
+        })
+    },
+    onSearchFocus(e) {
+    },
+    onSearchBlur(e) {
+    },
+    onSearchConfirm(e) {
+    },
+    onSearchClear(e) {
+        this.setData({
+            searchValue: '',
+        })
+    },
+    onSearchCancel(e) {
+    },
 });
