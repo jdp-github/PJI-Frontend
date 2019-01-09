cbd3c2860d9c36ce51beca4cde754b83f4b4b8e5
jdp-github <jdp_mail@163.com>
2019-01-09 22:13:31 +0800

    接口字段调整

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,134 +4,135 @@ const app = getApp();
 var constant = require('../../utils/constant.js');
 
 Page({
-    data: {
-        searchValue: '',
-        centerList: [],
-        centerTempList: [], // 存储中心列表，取消搜索时恢复数据用
-        visibleCenter: false,
-        centerName: ''
-    },
+  data: {
+    searchValue: '',
+    centerList: [],
+    centerTempList: [], // 存储中心列表，取消搜索时恢复数据用
+    visibleCenter: false,
+    centerName: ''
+  },
 
-    onLoad: function() {
-        this.initData()
-    },
+  onLoad: function() {
+    this.initData()
+  },
 
-    initData() {
-        var that = this
-        wx.showLoading({
-            title: '请求数据中...',
-        })
-        wx.request({
-            url: constant.basePath,
-            data: {
-                service: 'Center.GetStaffCenterList',
-                openid: app.globalData.openid
-            },
-            header: {
-                'content-type': 'application/json'
-            },
-            success(res) {
-                // console.log(JSON.stringify(res))
-                wx.hideLoading()
+  initData() {
+    var that = this
+    wx.showLoading({
+      title: '请求数据中...',
+    })
+    wx.request({
+      url: constant.basePath,
+      data: {
+        service: 'Center.GetStaffCenterList',
+        openid: app.globalData.openid
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        // console.log(JSON.stringify(res))
+        wx.hideLoading()
 
-                that.setData({
-                    centerList: res.data.data.list,
-                    centerTempList: res.data.data.list
-                })
-            },
-            fail(res) {
-                wx.hideLoading()
-            }
+        that.setData({
+          centerList: res.data.data.list,
+          centerTempList: res.data.data.list
         })
-    },
+      },
+      fail(res) {
+        wx.hideLoading()
+      }
+    })
+  },
 
-    // 点击人员
-    onStaffClick(e) {
-        wx.navigateTo({
-            url: '/people/people?centerId=' + e.target.dataset.centerId
-        })
-    },
+  // 点击人员
+  onStaffClick(e) {
+    wx.navigateTo({
+      url: '/people/people?centerId=' + e.target.dataset.centerId
+    })
+  },
 
-    // 点击病历
-    onRecordClick(e) {},
+  // 点击病历
+  onRecordClick(e) {},
 
-    // ------------------ search begin ------------------ //
-    onSearchChange(e) {
-        wx.request({
-            url: constant.basePath,
-            data: {
-                service: 'Center.SearchStaffCenter',
-                openid: app.globalData.openid,
-                keyword: e.detail.value
-            },
-            header: {
-                'content-type': 'application/json'
-            },
-            success(res) {
-                // console.log(JSON.stringify(res))
-                wx.hideLoading()
-                that.setData({
-                    centerList: res.data.data.list
-                })
-            },
-            fail(res) {
-                // wx.hideLoading()
-            }
+  // ------------------ search begin ------------------ //
+  onSearchChange(e) {
+    wx.request({
+      url: constant.basePath,
+      data: {
+        service: 'Center.SearchStaffCenter',
+        openid: app.globalData.openid,
+        keyword: e.detail.value
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        // console.log(JSON.stringify(res))
+        wx.hideLoading()
+        that.setData({
+          centerList: res.data.data.list
         })
-    },
-    onSearchFocus(e) {},
-    onSearchBlur(e) {},
-    onSearchConfirm(e) {},
-    onSearchClear(e) {
-        this.setData({
-            centerList: this.data.centerTempList
-        })
-    },
-    onSearchCancel(e) {},
-    // ------------------ search end ------------------ //
+      },
+      fail(res) {
+        // wx.hideLoading()
+      }
+    })
+  },
+  onSearchFocus(e) {},
+  onSearchBlur(e) {},
+  onSearchConfirm(e) {},
+  onSearchClear(e) {
+    this.setData({
+      centerList: this.data.centerTempList
+    })
+  },
+  onSearchCancel(e) {},
+  // ------------------ search end ------------------ //
 
-    // ------------------ add begin ------------------ //
-    addCenter(e) {
-        this.setData({
-            visibleCenter: true,
-        });
-    },
-    okAddCenter() {
-        var that = this;
-        if (that.data.centerName.length == 0) {
-            wx.showToast({
-                title: '请输入中心名',
-            })
-            return
-        }
-        wx.showLoading({
-            title: '新增中心...',
-        })
-        wx.request({
-            url: constant.basePath,
-            data: {
-                service: 'Center.CreateCenter',
-                openid: app.globalData.openid,
-                center_name: that.data.centerName
-            },
-            header: {
-                'content-type': 'application/json'
-            },
-            success(res) {
-                // console.log(JSON.stringify(res))
-                wx.hideLoading()
-                that.initData()
-            },
-            fail(res) {
-                wx.hideLoading()
-            }
-        })
-    },
-    onCenterClose(e) {},
-    closeCenter(e) {
-        this.setData({
-            visibleCenter: false,
-        })
-    },
-    // ------------------ add end ------------------ //
+  // ------------------ add begin ------------------ //
+  addCenter(e) {
+    this.setData({
+      visibleCenter: true,
+    });
+  },
+  okAddCenter() {
+    var that = this;
+    if (that.data.centerName.length == 0) {
+      wx.showToast({
+        icon: 'none',
+        title: '请输入中心名',
+      })
+      return
+    }
+    wx.showLoading({
+      title: '新增中心...',
+    })
+    wx.request({
+      url: constant.basePath,
+      data: {
+        service: 'Center.CreateCenter',
+        openid: app.globalData.openid,
+        center_name: that.data.centerName
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        // console.log(JSON.stringify(res))
+        wx.hideLoading()
+        that.initData()
+      },
+      fail(res) {
+        wx.hideLoading()
+      }
+    })
+  },
+  onCenterClose(e) {},
+  closeCenter(e) {
+    this.setData({
+      visibleCenter: false,
+    })
+  },
+  // ------------------ add end ------------------ //
 });
--- pages/index/people/people.js
+++ pages/index/people/people.js
@@ -3,211 +3,211 @@ const app = getApp();
 var constant = require('../../../utils/constant.js');
 
 Page({
-    data: {
-        isAdmin: 0,
-        centerId: '',
-        roleList: [],
-        staffList: [],
-        staffTempList: [], // 存储数据，搜索清空是恢复数据用
-        searchValue: '',
-        visiblePeople: false,
-        filterItems: [{
-                type: 'sort',
-                label: '姓名',
-                value: 'name',
-                groups: ['001'],
-            },
-            {
-                type: 'sort',
-                label: '授权日期',
-                value: 'authorization_date',
-                groups: ['004'],
-            },
-        ],
-    },
-    onLoad: function(options) {
-        this.setData({
-            centerId: options.centerId,
-            isAdmin: app.globalData.is_admin
+  data: {
+    isAdmin: 0,
+    centerId: '',
+    roleList: [],
+    staffList: [],
+    staffTempList: [], // 存储数据，搜索清空是恢复数据用
+    searchValue: '',
+    visiblePeople: false,
+    filterItems: [{
+        type: 'sort',
+        label: '姓名',
+        value: 'name',
+        groups: ['001'],
+      },
+      {
+        type: 'sort',
+        label: '授权日期',
+        value: 'authorization_date',
+        groups: ['004'],
+      },
+    ],
+  },
+  onLoad: function(options) {
+    this.setData({
+      centerId: options.centerId,
+      isAdmin: app.globalData.is_admin
+    })
+    // this.initData();
+    this.requestRoleList()
+    this.getUsers()
+  },
+
+  getUsers(params = {}) {
+    wx.showLoading();
+    this.setData({
+      staffList: [{
+          staff_avatar: "https://avatars3.githubusercontent.com/u/36479205?s=400&u=cb3d4cf7f58f5cfe4602199485cfec3b60527d08&v=4",
+          staff_name: "李文浩",
+          center: '骨头中心',
+          role_name: '中心管理员',
+          auth_time: "2018-12-24"
+        },
+        {
+          staff_avatar: "https://avatars3.githubusercontent.com/u/36479205?s=400&u=cb3d4cf7f58f5cfe4602199485cfec3b60527d08&v=4",
+          staff_name: "季大鹏",
+          center: '骨头中心',
+          role_name: '项目负责人',
+          auth_time: "2018-12-24"
+        }
+      ]
+    });
+    wx.hideLoading();
+  },
+
+  // ------------- filter begin ------------- //
+  onFilterChange(e) {
+    const checkedItems = e.detail.checkedItems;
+    const params = {};
+
+    checkedItems.forEach((n) => {
+      if (n.checked) {
+        if (n.value === 'name') {
+          params.sort = n.value;
+          params.order = n.sort === 1 ? 'asc' : 'desc'
+        } else if (n.value === 'role') {
+          params.sort = n.value;
+          params.order = n.sort === 1 ? 'asc' : 'desc'
+        }
+      }
+    });
+  },
+
+  onFilterOpen(e) {
+    this.setData({
+      pageStyle: 'height: 100%; overflow: hidden',
+    })
+  },
+  onFilterClose(e) {
+    this.setData({
+      pageStyle: '',
+    })
+  },
+  // ------------- filter end ------------- //
+
+  // ------------- search begin ------------- //
+  onSearchChange(e) {
+    var that = this
+    wx.request({
+      url: constant.basePath,
+      data: {
+        service: 'Center.SearchCenterMember',
+        center_id: that.data.centerId,
+        keyword: 'e.detail.value'
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        // console.log(JSON.stringify(res))
+        wx.hideLoading()
+        that.setData({
+          staffList: res.data.data.list
         })
-        // this.initData();
-        this.requestRoleList()
-        this.getUsers()
-    },
+      },
+      fail(res) {
+        wx.hideLoading()
+      }
+    })
+  },
+  onSearchFocus(e) {},
+  onSearchBlur(e) {},
+  onSearchConfirm(e) {},
+  onSearchClear(e) {
+    this.setData({
+      staffList: this.data.staffTempList
+    });
+  },
+  onSearchCancel(e) {},
+  // ------------- search end ------------- //
 
-    getUsers(params = {}) {
-        wx.showLoading();
-        this.setData({
-            staffList: [{
-                    staff_avatar: "https://avatars3.githubusercontent.com/u/36479205?s=400&u=cb3d4cf7f58f5cfe4602199485cfec3b60527d08&v=4",
-                    staff_name: "李文浩",
-                    center: '骨头中心',
-                    role_name: '中心管理员',
-                    auth_time: "2018-12-24"
-                },
-                {
-                    staff_avatar: "https://avatars3.githubusercontent.com/u/36479205?s=400&u=cb3d4cf7f58f5cfe4602199485cfec3b60527d08&v=4",
-                    staff_name: "季大鹏",
-                    center: '骨头中心',
-                    role_name: '项目负责人',
-                    auth_time: "2018-12-24"
-                }
-            ]
-        });
-        wx.hideLoading();
-    },
+  // ------------- pop begin ------------- //
+  operationBtn(e) {
+    this.setData({
+      visiblePeople: true,
+    });
+  },
+  onRoleChange(e) {
+    console.log(e)
+    this.setData({
+      visiblePeople: false,
+    });
+  },
+  // onPeopleClose(e) {},
+  // // 保存
+  // popSave() {
+  //     this.setData({
+  //         visiblePeople: false,
+  //     })
+  // },
+  // // 删除
+  // popDele() {
+  //     this.setData({
+  //         visiblePeople: false,
+  //     })
+  // },
+  // // 取消
+  // popCancel(e) {
+  //     this.setData({
+  //         visiblePeople: false,
+  //     })
+  // },
+  // ------------- pop end ------------- //
 
-    // ------------- filter begin ------------- //
-    onFilterChange(e) {
-        const checkedItems = e.detail.checkedItems;
-        const params = {};
+  initData() {
+    this.requestRoleList()
+    this.requestCenterStaffList()
+  },
 
-        checkedItems.forEach((n) => {
-            if (n.checked) {
-                if (n.value === 'name') {
-                    params.sort = n.value;
-                    params.order = n.sort === 1 ? 'asc' : 'desc'
-                } else if (n.value === 'role') {
-                    params.sort = n.value;
-                    params.order = n.sort === 1 ? 'asc' : 'desc'
-                }
-            }
-        });
-    },
-
-    onFilterOpen(e) {
-        this.setData({
-            pageStyle: 'height: 100%; overflow: hidden',
+  // 角色列表
+  requestRoleList() {
+    var that = this
+    wx.request({
+      url: constant.basePath,
+      data: {
+        service: 'Center.GetCenterRole'
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        console.log(JSON.stringify(res))
+        that.setData({
+          roleList: res.data.data.list
         })
-    },
-    onFilterClose(e) {
-        this.setData({
-            pageStyle: '',
-        })
-    },
-    // ------------- filter end ------------- //
+      },
+      fail(res) {}
+    })
+  },
 
-    // ------------- search begin ------------- //
-    onSearchChange(e) {
-        var that = this
-        wx.request({
-            url: constant.basePath,
-            data: {
-                service: 'Center.SearchCenterMember',
-                center_id: that.data.centerId,
-                keyword: 'e.detail.value'
-            },
-            header: {
-                'content-type': 'application/json'
-            },
-            success(res) {
-                // console.log(JSON.stringify(res))
-                wx.hideLoading()
-                that.setData({
-                    staffList: res.data.data.list
-                })
-            },
-            fail(res) {
-                wx.hideLoading()
-            }
+  // 中心人员
+  requestCenterStaffList() {
+    var that = this
+    wx.showLoading({
+      title: '请求数据中...',
+    })
+    wx.request({
+      url: constant.basePath,
+      data: {
+        service: 'Center.GetCenterMember',
+        center_id: that.data.centerId
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        // console.log(JSON.stringify(res))
+        wx.hideLoading()
+        that.setData({
+          staffList: res.data.data.list,
+          staffTempList: res.data.data.list
         })
-    },
-    onSearchFocus(e) {},
-    onSearchBlur(e) {},
-    onSearchConfirm(e) {},
-    onSearchClear(e) {
-        this.setData({
-            staffList: this.data.staffTempList
-        });
-    },
-    onSearchCancel(e) {},
-    // ------------- search end ------------- //
-
-    // ------------- pop begin ------------- //
-    operationBtn(e) {
-        this.setData({
-            visiblePeople: true,
-        });
-    },
-    onRoleChange(e) {
-        console.log(e)
-        this.setData({
-            visiblePeople: false,
-        });
-    },
-    // onPeopleClose(e) {},
-    // // 保存
-    // popSave() {
-    //     this.setData({
-    //         visiblePeople: false,
-    //     })
-    // },
-    // // 删除
-    // popDele() {
-    //     this.setData({
-    //         visiblePeople: false,
-    //     })
-    // },
-    // // 取消
-    // popCancel(e) {
-    //     this.setData({
-    //         visiblePeople: false,
-    //     })
-    // },
-    // ------------- pop end ------------- //
-
-    initData() {
-        this.requestRoleList()
-        this.requestCenterStaffList()
-    },
-
-    // 角色列表
-    requestRoleList() {
-        var that = this
-        wx.request({
-            url: constant.basePath,
-            data: {
-                service: 'Center.GetCenterRole'
-            },
-            header: {
-                'content-type': 'application/json'
-            },
-            success(res) {
-                console.log(JSON.stringify(res))
-                that.setData({
-                    roleList: res.data.data.list
-                })
-            },
-            fail(res) {}
-        })
-    },
-
-    // 中心人员
-    requestCenterStaffList() {
-        var that = this
-        wx.showLoading({
-            title: '请求数据中...',
-        })
-        wx.request({
-            url: constant.basePath,
-            data: {
-                service: 'Center.GetCenterMember',
-                center_id: that.data.centerId
-            },
-            header: {
-                'content-type': 'application/json'
-            },
-            success(res) {
-                // console.log(JSON.stringify(res))
-                wx.hideLoading()
-                that.setData({
-                    staffList: res.data.data.list,
-                    staffTempList: res.data.data.list
-                })
-            },
-            fail(res) {
-                wx.hideLoading()
-            }
-        })
-    }
+      },
+      fail(res) {
+        wx.hideLoading()
+      }
+    })
+  }
 });
