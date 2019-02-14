709edc8965dd111e3bce2a1638de945019760f3d
jdp-github <jdp_mail@163.com>
2019-02-14 22:37:39 +0800

    标本列表

--- pages/specimen/specimen.js
+++ pages/specimen/specimen.js
@@ -5,36 +5,37 @@ var util = require('../../utils/util.js');
 
 Page({
     data: {
-        filterItems: [
-            {
-              type: 'sort',
-              label: '名称',
-              value: 'name',
-              groups: ['001'],
+        filterItems: [{
+                type: 'sort',
+                label: '名称',
+                value: 'name',
+                groups: ['001'],
             },
             {
-              type: 'sort',
-              label: '启用时间',
-              value: 'create_date',
-              groups: ['002'],
-          },
-          {
-              type: 'sort',
-              label: '修改时间',
-              value: 'update_date',
-              groups: ['003'],
-          },
-          {
-              type: 'sort',
-              label: '锁定',
-              value: 'lock',
-              groups: ['004'],
-          },
+                type: 'sort',
+                label: '启用时间',
+                value: 'create_date',
+                groups: ['002'],
+            },
+            {
+                type: 'sort',
+                label: '修改时间',
+                value: 'update_date',
+                groups: ['003'],
+            },
+            {
+                type: 'sort',
+                label: '锁定',
+                value: 'lock',
+                groups: ['004'],
+            },
         ],
         boxList: ['2019第一盒'],
         visibleBox: false,
         boxName: ''
     },
+
+
     addBox(e) {
         this.setData({
             visibleBox: true,
@@ -77,4 +78,12 @@ Page({
             visibleBox: false
         })
     },
+
+    onLoad: function(options) {
+        this.initData()
+    },
+
+    initData(){
+        
+    }
 });
