16084f9ba931c5c2dbd664f03fc6c4574ee2d49d
Jason-ZW <zhenyang@rancher.com>
2019-02-19 19:22:49 +0800

    增加select到标本详情

--- pages/specimen/detail/detail.js
+++ pages/specimen/detail/detail.js
@@ -1,5 +1,9 @@
 "use strict";
 
+import {
+    $wuxSelect,
+} from '../../../miniprogram_npm/wux-weapp/index'
+
 const app = getApp();
 var constant = require('../../../utils/constant.js');
 var util = require('../../../utils/util.js');
@@ -7,6 +11,12 @@ var util = require('../../../utils/util.js');
 Page({
     data: {
         specimenInfoVisible: false,
+        infectTitle: '请选择是否感染',
+        infectValue: '',
+        typeTitle: '请选择类型',
+        typeValue: '',
+        ownerTitle: '请选择存放者',
+        ownerValue: '',
     },
     showSpecimenInfo: function() {
         this.setData({
@@ -28,6 +38,60 @@ Page({
     },
     onClosedSpecimenInfo() {
         console.log('onClosedSpecimenInfo')
+    },
+    onClickInfect() {
+        $wuxSelect('#selectInfect').open({
+            value: this.data.infectValue,
+            options: [
+                "清空",
+                '是',
+                '否',
+            ],
+            onConfirm: (value, index, options) => {
+                if (index !== -1) {
+                    this.setData({
+                        infectValue: value,
+                        infectTitle: options[index],
+                    })
+                }
+            },
+        })
+    },
+    onClickType() {
+        $wuxSelect('#selectType').open({
+            value: this.data.typeValue,
+            options: [
+                "清空",
+                '置换术后',
+                '占位器',
+            ],
+            onConfirm: (value, index, options) => {
+                if (index !== -1) {
+                    this.setData({
+                        typeValue: value,
+                        typeTitle: options[index],
+                    })
+                }
+            },
+        })
+    },
+    onClickOwner() {
+        $wuxSelect('#selectOwner').open({
+            value: this.data.ownerValue,
+            options: [
+                "清空",
+                '小王',
+                '小雷',
+            ],
+            onConfirm: (value, index, options) => {
+                if (index !== -1) {
+                    this.setData({
+                        ownerValue: value,
+                        ownerTitle: options[index],
+                    })
+                }
+            },
+        })
     },
 });
 
