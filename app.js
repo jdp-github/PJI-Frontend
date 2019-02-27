0eefa5434c0c33180868a8772d5c177f2571e4f4
Jason-ZW <zhenyang@rancher.com>
2019-02-27 17:56:12 +0800

    Re-design auth page

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 "use strict";
 App({
-    onLaunch: function() {
-        // wx.getSetting({
-        //     success: res => {
-        //         if (res.authSetting['scope.userInfo']) {
-        //             wx.getUserInfo({
-        //                 success: res => {
-        //                     this.globalData.userInfo = res.userInfo;
-        //                     if (this.userInfoReadyCallback) {
-        //                         this.userInfoReadyCallback(res)
-        //                     }
-        //                 }
-        //             });
-        //         }
-        //     }
-        // });
+    onLunch: function () {
+        wx.getSystemInfo({
+            success: e => {
+                this.globalData.StatusBar = e.statusBarHeight;
+                let custom = wx.getMenuButtonBoundingClientRect();
+                this.globalData.Custom = custom;
+                this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
+            }
+        })
     },
     globalData: {
         is_admin: '', // 管理员flag 0否，1是
--- utils/constant.js
+++ utils/constant.js
@@ -1,8 +1,8 @@
-// utils/constant.js
+'use strict';
 let basePath = "https://pji.think-show.com/api/Public/PJI/";
 let response_success = 0;
 
 module.exports = {
     basePath: basePath,
     response_success: response_success
-}
+};
