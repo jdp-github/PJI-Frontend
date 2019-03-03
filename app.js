f2c65a566d8cc1c09b7b388a9d0bb7b5eb82abf2
Jason-ZW <zhenyang@rancher.com>
2019-03-03 16:48:06 +0800

    Re-design stats page

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ let eventEmitter = require('events');
 let emitter = new eventEmitter();
 
 App({
-    onLaunch: function () {
-        wx.getSystemInfo({
-            success: e => {
-                this.globalData.StatusBar = e.statusBarHeight;
-                let custom = wx.getMenuButtonBoundingClientRect();
-                this.globalData.Custom = custom;
-                this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
-            }
-        });
-    },
-    globalData: {
-        is_admin: '', // 管理员flag 0否，1是
-        openid: '',
-        avatarUrl: '',
-        nickName: '',
-        Custom: '',
-        CustomBar: '',
-        StatusBar: '',
-        selected: 0,
-        emitter: emitter
-    }
+  onLaunch: function() {
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.StatusBar = e.statusBarHeight;
+        let custom = wx.getMenuButtonBoundingClientRect();
+        this.globalData.Custom = custom;
+        this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
+      }
+    });
+  },
+  globalData: {
+    is_admin: '', // 管理员flag 0否，1是
+    openid: '',
+    avatarUrl: '',
+    nickName: '',
+    Custom: '',
+    CustomBar: '',
+    StatusBar: '',
+    selected: 0,
+    emitter: emitter
+  }
 });
--- pages/stats/stats.js
+++ pages/stats/stats.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let constant = require('../../utils/constant.js');
+let regeneratorRuntime = require('../../lib/regenerator-runtime/runtime');
 
 const app = getApp();
 
@@ -12,6 +13,23 @@ Page({
         CustomBar: app.globalData.CustomBar,
         Custom: app.globalData.Custom,
         emitter: null,
+        centerObjList: [],
+        centerValueList: [],
+        centerValue: '全部',
+        centerIndex: 0,
+        infectEc: {
+            disableTouch: true
+        },
+        typeEc: {
+            disableTouch: true
+        },
+        completeEc: {
+            disableTouch: true
+        },
+        noInfectPercentage: 0,
+        seizePercentage: 0,
+        approvePercentage: 0,
+        notApprovePercentage: 0,
     },
     onHide: function () {
         this.setData({
@@ -29,6 +47,14 @@ Page({
                 console.log("stats modal");
             });
         }
+    },
+    onLoad: function () {
+        this.loadProgress();
+        this.initLogic();
+    },
+    initLogic: async function () {
+        await this.requestCenterList();
+        await this.requestCharts(true);
     },
     loadProgress: function () {
         if (this.data.loadProgress < 96) {
@@ -69,17 +95,103 @@ Page({
             });
         }, 1500);
     },
-    onClickMember: function (e) {
-        wx.navigateTo({
-            url: '../center/member/member?centerId=' + e.target.dataset.centerid
-        });
-    },
-    onClickSpecimen: function (e) {
-        console.log("specimen");
-    },
-    backToAuth: function() {
+    backToAuth: function () {
         wx.navigateTo({
             url: '../auth/auth'
+        });
+    },
+    centerChange: function (e) {
+        this.setData({
+            centerIndex: e.detail.value,
+            centerValue: this.data.centerValueList[e.detail.value]
+        });
+        if (this.data.centerValue == "全部") {
+            this.requestCharts(true);
+        } else {
+            this.requestCharts(false);
+        }
+    },
+    requestCenterList: function () {
+        let that = this;
+        return new Promise((resolve, reject) => {
+            wx.request({
+                url: constant.basePath,
+                data: {
+                    service: 'Center.GetCenterList'
+                },
+                header: {
+                    'content-type': 'application/json'
+                },
+                success(res) {
+                    if (res.data.data.code == constant.response_success) {
+                        that.setData({
+                            centerObjList: res.data.data.list
+                        });
+                        for (let i = 0, len = that.data.centerObjList.length; i < len; i++) {
+                            that.data.centerValueList[i] = that.data.centerObjList[i].name;
+                        }
+                        that.data.centerValueList.push('全部');
+                        that.setData({
+                            centerValueList: that.data.centerValueList
+                        });
+                    }
+                    that.completeProgress();
+                    resolve(res);
+                },
+                fail(res) {
+                    that.completeProgress();
+                    that.showToast({
+                        msg: res.data.msg
+                    });
+                    reject(res);
+                }
+            });
+        });
+    },
+    requestCharts: function (all) {
+        let that = this;
+        return new Promise((resolve, reject) => {
+            wx.request({
+                url: constant.basePath,
+                data: {
+                    service: 'Statistics.GetCharts',
+                    center_id: all ? "" : that.data.centerObjList[that.data.centerIndex].id
+                },
+                header: {
+                    'content-type': 'application/json'
+                },
+                success(res) {
+                    if (res.data.data.code == constant.response_success) {
+                        let notInfect = res.data.data.infect_list.notinfect;
+                        let infect = res.data.data.infect_list.infect;
+                        let seize = res.data.data.type_list.seize;
+                        let displace = res.data.data.type_list.displace;
+                        let approve = res.data.data.finished_list.approve;
+                        let notApprove = res.data.data.finished_list.notapprove;
+                        let notCompleted = res.data.data.finished_list.notcomplete;
+                        that.setData({
+                            complete_list: res.data.data.finished_list,
+                            infect_list: res.data.data.infect_list,
+                            type_list: res.data.data.type_list,
+                            noInfectPercentage: Math.round((parseInt(notInfect) / (parseInt(notInfect) + parseInt(infect))) * 100),
+                            seizePercentage: Math.round((parseInt(seize) / (parseInt(seize) + parseInt(displace))) * 100),
+                            approvePercentage: Math.round((parseInt(approve) / (parseInt(approve) + parseInt(notApprove) + parseInt(notCompleted))) * 100),
+                            notApprovePercentage: Math.round((notApprove / (parseInt(approve) + parseInt(notApprove) + parseInt(notCompleted))) * 100)
+                        });
+                    } else {
+                        that.showToast({
+                            msg: res.data.data.msg,
+                        });
+                    }
+                    resolve(res);
+                },
+                fail(res) {
+                    that.showToast({
+                        msg: res.data.msg
+                    });
+                    reject(res);
+                }
+            });
         });
     }
 });
