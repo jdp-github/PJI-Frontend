d2697898ba9ca3971cff0aaa7e1df6b806df9765
jidp <ribunkou@gmail.com>
2019-12-25 17:34:48 +0800

    人员

---
+++ pages/center/manage/member/member.js
@@ -0,0 +1,101 @@
+'use strict';
+
+let constant = require('../../../../utils/constant.js');
+let util = require('../../../../utils/util.js');
+
+const app = getApp();
+
+Page({
+    data: {
+        loadProgress: 0,
+        loadModal: false,
+        StatusBar: app.globalData.StatusBar,
+        CustomBar: app.globalData.CustomBar,
+        Custom: app.globalData.Custom,
+        // -------- modal begin ----------- //
+        modalName: '',
+        errMsg: '',
+        // -------- modal end ------------- //
+    },
+
+    // -------- 提示框 begin -------- //
+    loadProgress: function() {
+        if (this.data.loadProgress < 96) {
+            this.setData({
+                loadProgress: this.data.loadProgress + 3
+            });
+        }
+        if (this.data.loadProgress < 100) {
+            setTimeout(() => {
+                this.loadProgress();
+            }, 100);
+        } else {
+            this.setData({
+                loadProgress: 0
+            });
+        }
+    },
+    completeProgress: function() {
+        this.setData({
+            loadProgress: 100
+        });
+    },
+    showToast: function(msg) {
+        wx.showToast({
+            icon: 'none',
+            title: msg,
+        });
+    },
+    showLoading: function() {
+        this.setData({
+            loadModal: true
+        });
+    },
+    hideLoading: function() {
+        setTimeout(() => {
+            this.setData({
+                loadModal: false
+            });
+        }, 1500);
+    },
+    // -------- 提示框 end -------- //
+
+    // -------- 模态对话框 start -------- //
+    showModal: function(e, errMessage = '') {
+        if (e.currentTarget) {
+            this.setData({
+                modalName: e.currentTarget.dataset.target
+            });
+        } else {
+            this.setData({
+                modalName: e,
+                errMsg: errMessage
+            });
+        }
+    },
+    hideModal: function(e) {
+        this.setData({
+            modalName: null
+        });
+    },
+    // -------- 模态对话框 end  -------- //
+
+    onLoad: function(options) {
+        // this.loadProgress();
+        // this.setData({
+        //     isAdmin: app.globalData.is_admin == '1',
+        //     centerId: options.centerId,
+        //     centerName: options.centerName,
+        //     caseId: options.caseId,
+        //     itemId: options.itemId,
+        //     isCreate: options.itemId == 0,
+        // });
+        // this.requestCaseInfo(this.data.caseId)
+        // this.setData({
+        //     addAvatar: app.globalData.avatarUrl
+        // })
+        // this.completeProgress();
+    },
+
+
+});
