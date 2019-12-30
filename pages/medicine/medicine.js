83ad31c1853bbb10833d69a807c152a33f228dc2
Jason-ZW <zhenyang@rancher.com>
2019-12-30 14:36:04 +0800

    加入用药

---
+++ pages/medicine/medicine.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let constant = require('../../utils/constant.js');
+let util = require('../../utils/util.js');
+
+const app = getApp();
+
+Page({
+    data: {
+        loadProgress: 0,
+        loadModal: false,
+        StatusBar: app.globalData.StatusBar,
+        CustomBar: app.globalData.CustomBar,
+        Custom: app.globalData.Custom,
+        isAdmin: false
+    },
+});
